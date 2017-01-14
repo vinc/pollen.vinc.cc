58a12e74111df06ce3d74370ada16bfc0c3ee8c7
Vincent Ollivier <v@vinc.cc>
2017-01-14 00:41:27 +0000

    Initial commit

---
+++ public/javascripts/plot.js
@@ -0,0 +1,142 @@
+$(function() {
+
+  var updateMap = function(site) {
+    console.debug("updating map");
+
+    $.getJSON("/api/sites/" + site, function(res) {
+      //var marker = L.marker([res.latdd, res.londd]).addTo(map);
+
+      $("#site-title").text(res.sitename + " (" + res.sigle + ")");
+      $("#site-elevation").text(res.elevation);
+    });
+  };
+
+  var updatePlot = function(site, limit) {
+    console.debug("updating plot");
+
+    $.getJSON("/api/sites/" + site + "/samples", function(res) {
+      var taxons = { };
+
+      res = res.sort(function(a, b) {
+        return a.agebp > b.agebp;
+      }).map(function(row) {
+        return {
+          label: row.varname,
+          x: row.agebp,
+          y: row.count
+        };
+      });
+
+      var counts = res.reduce(function(acc, row) {
+        acc[row.label] = (acc[row.label] || 0) + row.y;
+        return acc;
+      }, {});
+
+      //numberOfTaxons = Object.keys(counts).length;
+
+      var datasets = Object.keys(counts).map(function(label, i) {
+        return {
+          label: label,
+          data: res.filter(function(row) { return row.label == label; }),
+          count: counts[label]
+        };
+      }).sort(function(a, b) {
+        return a.count < b.count;
+      }).slice(0, limit).map(function(row, i) {
+        return Object.assign(row, {
+          fill: false,
+          borderColor:          "hsla(" + i * 360 / limit + ", 50%, 60%, 0.50)",
+          pointBorderColor:     "hsla(" + i * 360 / limit + ", 50%, 60%, 0.75)",
+          pointBackgroundColor: "hsla(" + i * 360 / limit + ", 50%, 60%, 0.75)"
+        });
+      });
+
+      var data = {
+        datasets: datasets
+      };
+
+      var options = {
+        animation: false,
+        scales: {
+          xAxes: [
+            {
+              type: "linear",
+              position: "bottom"
+            }
+          ]
+        }
+      };
+
+      var ctx = $("#plot");
+      var plot = new Chart(ctx, {
+        type: "line",
+        //xAxisID: "agebp",
+        //yAxisID: "count",
+        data: data,
+        options: options
+      });
+    });
+  };
+
+  var map = L.map("map", {
+    center: [48, -2],
+    zoom: 7
+  });
+  
+  var layer = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution: '<a href="http://osm.org/copyright/">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  var numberOfTaxons = 10; // TODO
+  var site;
+  var limit;
+
+  $.getJSON("/api/sites", function(res) {
+    res.sort(function(a, b) {
+      return a.sitename + a.sigle > b.sitename + b.sigle;
+    }).forEach(function(row) {
+      var title = row.sitename + " (" + row.sigle + ")";
+      var value = row.sigle;
+
+      $("#select-site").append(new Option(title, value));
+
+      if (-10 < row.londd && row.londd < 0) {
+        if (46 < row.latdd && row.latdd < 50) {
+          L.marker([row.latdd, row.londd], { icon: L.divIcon() })
+            .addTo(map)
+            .on("click", function(e) {
+              console.debug("click on marker '" + value + "'");
+              $("#select-site").val(value);
+              site = row.sigle;
+              updatePlot(site, limit);
+              updateMap(site);
+            });
+        }
+      }
+    });
+
+    site = res[0].sigle; // TODO: check res.length
+    limit = 1; // TODO: check number of taxons, then use checkbox
+
+    updatePlot(site, limit);
+    updateMap(site);
+  });
+
+  // TODO: Update it
+  for (var i = 1; i <= numberOfTaxons; i++) { 
+    $("#select-taxons").append(new Option(i, i)); 
+  }
+
+  $("#select-site").change(function() {
+    site = $("#select-site").val();
+    console.debug("changing site: " + site);
+    updatePlot(site, limit);
+    updateMap(site);
+  });
+
+  $("#select-taxons").change(function() {
+    limit = $("#select-taxons").val();
+    console.debug("changing limit: " + limit);
+    updatePlot(site, limit);
+  });
+});
---
+++ routes/sites.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var pg = require('pg');
+
+var router = express.Router();
+var client = new pg.Client();
+
+var config = {
+  user: 'v', //env var: PGUSER
+  database: 'epd', //env var: PGDATABASE
+  password: '', //env var: PGPASSWORD
+  host: 'localhost', // Server hosting the postgres database
+  port: 5432, //env var: PGPORT
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+var pool = new pg.Pool(config);
+
+/* GET sites listing. */
+router.get('/', function(req, res, next) {
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(`
+      SELECT DISTINCT entity.sigle, siteloc.sitename, siteloc.latdd, siteloc.londd, siteloc.elevation
+        FROM entity, siteloc
+        WHERE
+          entity.site_ = siteloc.site_
+        ORDER BY entity.sigle
+      `, function(err, result) {
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      res.json(result.rows);
+      done(); // release the client back to the pool
+    });
+  });
+});
+
+router.get('/:sigle', function(req, res, next) {
+  var sigle = req.params.sigle.toUpperCase();
+
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(`
+      SELECT DISTINCT entity.sigle, siteloc.sitename, siteloc.latdd, siteloc.londd, siteloc.elevation
+        FROM entity, siteloc
+        WHERE
+          entity.sigle    = $1 AND
+          entity.site_     = siteloc.site_
+      `, [sigle], function(err, result) {
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      res.json(result.rows[0]);
+      done(); // release the client back to the pool
+    });
+  });
+});
+
+router.get('/:sigle/samples', function(req, res, next) {
+  var sigle = req.params.sigle.toUpperCase();
+
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query(`
+      SELECT DISTINCT p_vars.varname, p_agedpt.agebp, p_counts.count, 100 * p_counts.count / sum(p_counts.count) OVER (PARTITION BY p_agedpt.agebp) AS percent
+        FROM entity, p_vars, p_agedpt, p_counts, p_group
+        WHERE
+          entity.sigle    = $1 AND
+          p_group.groupid  = $2 AND
+          entity.e_        = p_counts.e_ AND
+          entity.e_        = p_agedpt.e_ AND
+          p_vars.var_      = p_group.var_ AND
+          p_vars.var_      = p_counts.var_ AND
+          p_counts.sample_ = p_agedpt.sample_
+        ORDER BY p_agedpt.agebp
+      `, [sigle, 'TRSH'], function(err, result) {
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      res.json(result.rows);
+      done(); // release the client back to the pool
+    });
+  });
+});
+
+module.exports = router;
