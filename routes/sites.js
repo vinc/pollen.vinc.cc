d8aa84a31fbf21ed1deadb43876c2928dd76b1f3
Vincent Ollivier <v@vinc.cc>
2017-01-16 09:30:01 +0000

    Move database querying code to sql module

--- routes/sites.js
+++ routes/sites.js
@@ -1,59 +1,19 @@
 var express = require('express');
-var pg = require('pg');
-var url = require('url');
-
 var sql = require('../sql');
-
-// The Pool constructor does not support passing a Database URL as the
-// parameter like `var client = new pg.Client(process.env.DATABASE_URL);`.
-
-var params = url.parse(process.env.DATABASE_URL);
-var auth = params.auth.split(':');
-var config = {
-  user: auth[0],
-  password: auth[1],
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-};
-var pool = new pg.Pool(config);
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  pool.connect(function(err, client, done) {
-    if(err) {
-      return console.error('error fetching client from pool', err);
-    }
-    // TODO: Remove unused attributes
-    client.query(sql('sites'), function(err, result) {
-
-      if(err) {
-        return console.error('error running query', err);
-      }
-
-      res.json(result.rows);
-      done(); // release the client back to the pool
-    });
+  sql('sites', function(err, result) {
+    res.json(result.rows);
   });
 });
 
 router.get('/:sigle', function(req, res, next) {
   var sigle = req.params.sigle.toUpperCase();
 
-  pool.connect(function(err, client, done) {
-    if(err) {
-      return console.error('error fetching client from pool', err);
-    }
-    client.query(sql('site'), [sigle], function(err, result) {
-
-      if(err) {
-        return console.error('error running query', err);
-      }
-
-      res.json(result.rows[0]);
-      done(); // release the client back to the pool
-    });
+  sql('site', [sigle], function(err, result) {
+    res.json(result.rows[0]);
   });
 });
 
@@ -61,19 +21,8 @@ router.get('/:sigle/samples', function(req, res, next) {
   var sigle = req.params.sigle.toUpperCase();
   var types = (req.query.types || "herb,trsh").toUpperCase().split(",");
 
-  pool.connect(function(err, client, done) {
-    if(err) {
-      return console.error('error fetching client from pool', err);
-    }
-    client.query(sql('site_samples'), [sigle, [types]], function(err, result) {
-
-      if(err) {
-        return console.error('error running query', err);
-      }
-
-      res.json(result.rows);
-      done(); // release the client back to the pool
-    });
+  sql('site_samples', [sigle, [types]], function(err, result) {
+    res.json(result.rows);
   });
 });
 
--- sql/index.js
+++ sql/index.js
@@ -1,5 +1,42 @@
 var fs = require('fs');
+var pg = require('pg');
+var url = require('url');
 
-module.exports = function(query) {
+// The Pool constructor does not support passing a Database URL as the
+// parameter like `var client = new pg.Client(process.env.DATABASE_URL);`.
+
+var params = url.parse(process.env.DATABASE_URL);
+var auth = params.auth.split(':');
+var config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+};
+var pool = new pg.Pool(config);
+
+var readQueryFile = function(query) {
   return fs.readFileSync('./sql/' + query + '.sql').toString();
+};
+
+module.exports = function(query, ...args) {
+  var callback = args.pop();
+  var parameters = args.pop();
+
+  pool.connect(function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    // TODO: Remove unused attributes
+    client.query(readQueryFile(query), parameters, function(err, result) {
+      done(); // release the client back to the pool
+
+      if(err) {
+        return console.error('error running query', err);
+      }
+
+      callback(err, result);
+    });
+  });
 };
