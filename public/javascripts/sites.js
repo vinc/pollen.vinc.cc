03ea85c84b2b4ef7a7126d2866e9d19e5e71f3c2
Vincent Ollivier <v@vinc.cc>
2017-01-17 22:10:17 +0000

    Add vue router and refactor client side script

--- public/javascripts/sites.js
+++ public/javascripts/sites.js
@@ -11,13 +11,13 @@ var createMap = function() {
 };
 
 var createMarkers = function(sites) {
-  var markers = L.markerClusterGroup({
+  var group = L.markerClusterGroup({
     showCoverageOnHover: false,
-    chunkedLoading: true,
+    //chunkedLoading: true, // FIXME bug if map.setView() is called at the same time
   });
 
   // Add markers on map
-  sites.forEach(function(site) {
+  var markers = sites.map(function(site) {
     var marker = L.marker([site.latdd, site.londd], {
       icon: L.divIcon({
         iconSize: [16, 16],
@@ -29,22 +29,20 @@ var createMarkers = function(sites) {
       vue.selectSite = site.sigle;
     });
 
-    markers.addLayer(marker);
+    return marker;
   });
 
-  return markers;
+  group.addLayers(markers);
+
+  return group;
 };
 
-var createChart = function(site, types, limit) {
-  // Remove previous chart
-  d3.select("#nvd3-container svg").remove();
-  d3.select(".nvtooltip").remove();
-
-  var querySite = site.toLowerCase();
+var fetchSamples = function({ sigle, types, limit }, callback) {
+  var querySite = sigle.toLowerCase();
   var queryTypes = types.join(",").toLowerCase();
-  var url = "/sites/" + querySite + "/samples?types=" + queryTypes;
+  var path = "/sites/" + querySite + "/samples?types=" + queryTypes;
 
-  Vue.http.get(url).then(response => {
+  Vue.http.get(path).then(response => {
     return response.json();
   }).then(json => {
     var counts = json.reduce(function(acc, row) {
@@ -57,10 +55,10 @@ var createChart = function(site, types, limit) {
       return {
         key: varname,
         values: json.filter(function(row) { return row.varname == varname; }),
-        count: counts[varname]
+        _count: counts[varname]
       };
     }).sort(function(a, b) {
-      return b.count - a.count;
+      return b._count - a._count;
     }).slice(0, limit).map(function(row, i) {
       row.color = "hsl(" + i * 360 / limit + ", 50%, 60%)";
       row.values = row.values.map(function(value) {
@@ -68,46 +66,69 @@ var createChart = function(site, types, limit) {
       });
       return row;
     });
-
-    nv.addGraph(function() {
-      var chart = nv.models.lineChart().options({
-        duration: 300,
-        useInteractiveGuideline: true
-      });
-
-      chart.interpolate("monotone");
-
-      // FIXME: only work for xAxis and not yAxis
-      //chart.padData(true);
-      //chart.padDataOuter(1);
-
-      chart.margin({ "top": 10, "right": 10 });
-
-      chart.xAxis
-        .axisLabel('Year cal BP')
-        .showMaxMin(false)
-        .tickPadding(5)
-        .tickFormat(d3.format(',r'));
-
-      chart.yAxis
-        .axisLabel('Pollen Count')
-        .showMaxMin(false)
-        .tickPadding(5)
-        .tickFormat(d3.format(',r'));
-
-      // Add new chart
-      d3.select('#nvd3-container').append('svg')
-        .datum(datum)
-        .call(chart);
-
-      //Update the chart when window resizes.
-      nv.utils.windowResize(function() {
-        chart.update();
-      });
-
-      return chart;
-    });
+    callback(null, datum);
   });
+};
+
+/*
+ * var datum = [
+ *   {
+ *     key: "Quercus", // Taxon name
+ *     color: "hsl(0, 50%, 60%),
+ *     values: [
+ *       { x: 1000, y: 42 }, // `x` is 'Year cal BP'; `y` is 'Pollen Count'
+ *       { x: 2000, y: 25 },
+ *       ...
+ *     ]
+ *   },
+ *   ...
+ * ]
+ *
+ */
+var createChart = function(datum) {
+  nv.addGraph(function() {
+    var chart = nv.models.lineChart().options({
+      duration: 300,
+      useInteractiveGuideline: true
+    });
+
+    chart.interpolate("monotone");
+
+    // FIXME: only work for xAxis and not yAxis
+    //chart.padData(true);
+    //chart.padDataOuter(1);
+
+    chart.margin({ "top": 10, "right": 10 });
+
+    chart.xAxis
+      .axisLabel('Year cal BP')
+      .showMaxMin(false)
+      .tickPadding(5)
+      .tickFormat(d3.format(',r'));
+
+    chart.yAxis
+      .axisLabel('Pollen Count')
+      .showMaxMin(false)
+      .tickPadding(5)
+      .tickFormat(d3.format(',r'));
+
+    // Add new chart
+    d3.select('#nvd3-container').append('svg')
+      .datum(datum)
+      .call(chart);
+
+    //Update the chart when window resizes.
+    nv.utils.windowResize(function() {
+      chart.update();
+    });
+
+    return chart;
+  });
+};
+
+var destroyChart = function() {
+  d3.select("#nvd3-container svg").remove();
+  d3.select(".nvtooltip").remove();
 };
 
 Vue.component("site-info", function (resolve, reject) {
@@ -121,8 +142,23 @@ Vue.component("site-info", function (resolve, reject) {
   });
 });
 
+var router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      name: "sites",
+      path: "/sites"
+    },
+    {
+      name: "site",
+      path: "/sites/:sigle"
+    }
+  ]
+});
+
 var vue = new Vue({
   el: "#app",
+  router: router,
   data: {
     selectSite: "",
     selectTypes: ["TRSH"],
@@ -130,39 +166,56 @@ var vue = new Vue({
     sites: [],
     site: undefined
   },
-  created: function() {
-  },
-  mounted: function() {
-    this.map = createMap();
+  mounted: function() { // Leaflet requires `mounted` instead of `created`
+    this.map = createMap(); // TODO: Create map later to avoid `setView()`
 
     this.$http.get("/sites").then(response => {
       return response.json();
     }).then(json => {
       this.sites = json;
-      this.map.addLayer(createMarkers(this.sites));
+      this.map.addLayer(createMarkers(json));
+      if (this.$route.name == "site") {
+        this.selectSite = this.$route.params.sigle.toUpperCase();
+      } // TODO: Create map in else case and below in place of setView
     });
   },
   watch: {
     selectSite: function(newSite) {
-      this.selectSite = newSite;
-
-      this.$http.get("/sites/" + newSite.toLowerCase()).then(response => {
+      var path = "/sites/" + newSite.toLowerCase();
+      this.$http.get(path).then(response => {
         return response.json();
       }).then(json => {
+        router.push(path);
         this.site = json;
+
+        // FIXME: This bug when chunkedLoading of markers is true
         this.map.setView([json.latdd, json.londd], 9);
-        createChart(this.selectSite, this.selectTypes, this.selectLimit);
+
+        this.updateChart();
       });
     },
     selectTypes: function(newTypes) {
       if (this.site) {
-        createChart(this.selectSite, this.selectTypes, this.selectLimit);
+        this.updateChart();
       }
     },
     selectLimit: function(newLimit) {
       if (this.site) {
-        createChart(this.selectSite, this.selectTypes, this.selectLimit);
+        this.updateChart();
       }
+    }
+  },
+  methods: {
+    updateChart() {
+      destroyChart();
+      var params = {
+        sigle: this.selectSite,
+        types: this.selectTypes,
+        limit: this.selectLimit
+      };
+      fetchSamples(params, function(err, samples) {
+        createChart(samples);
+      });
     }
   }
 });
